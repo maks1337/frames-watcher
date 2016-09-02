c907be62a0ee3894e2c5f26286ecb1fdfc94b6d7
Maksymilian Ratajczyk <kontakt@maksymilianratajczyk.pl>
2016-09-02 22:28:57 +0200

    Code quality fix for Context.ts

--- src/Context.ts
+++ src/Context.ts
@@ -12,39 +12,28 @@ namespace FrameWatcher {
         protected iframe: boolean = false;
 
         constructor(width: number= 0, height: number= 0) {
-
             this.setSize(width, height);
             this.setMetadata();
-
         }
 
         setMetadata(): void {
-
             this.iframe = ((window.location !== window.parent.location) ? true : false);
             this.url = window.location.href;
             this.domain = window.location.hostname;
-
         }
+
         getSize(): Object {
-
             return { width: this.width, height: this.height };
-
         }
 
         setSize(width: number, height: number): void {
-
             if (width > 0 && height > 0) {
-
                 this.width = width;
                 this.height = height;
-
             }else {
                 this.width = (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
                 this.height = (window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight);
             }
-
         }
-
     }
-
 }
