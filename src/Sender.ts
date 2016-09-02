478e3057388d9eaa975c615c7290b25ee0ca8610
Maksymilian Ratajczyk <kontakt@maksymilianratajczyk.pl>
2016-09-02 22:29:27 +0200

    Code quality fix for Sender.ts

--- src/Sender.ts
+++ src/Sender.ts
@@ -15,9 +15,7 @@ namespace FrameWatcher {
                 placement: element.id,
                 view: (viewId) ? viewId : this.uuid()
             };
-
             if (cookie) this._data["cookie"] = cookie;
-
             element.viewed.forEach((a) => this._data[`strat-${a[0]}`] = a[1]);
         }
 
@@ -44,8 +42,6 @@ namespace FrameWatcher {
                 lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & 0xff] + "-" + lut[d2 >> 16 & 0xff] + lut[d2 >> 24 & 0xff] +
                 lut[d3 & 0xff] + lut[d3 >> 8 & 0xff] + lut[d3 >> 16 & 0xff] + lut[d3 >> 24 & 0xff];
         }
-
-
     }
 
     export class Sender {
