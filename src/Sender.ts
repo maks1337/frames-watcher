c5947e78c28936caacacdd1892283d04c2cab309
Maksymilian Ratajczyk <kontakt@maksymilianratajczyk.pl>
2016-10-02 23:37:33 +0200

    UUID refactoring

--- src/Sender.ts
+++ src/Sender.ts
@@ -7,30 +7,14 @@ namespace FrameWatcher {
 
     type possibleSenders = HttpSender | SocketSender | ConsoleSender;
 
-    export class Data {
-        private _data: Object;
-        constructor(element: Element, cookie: string, viewId: string) {
-            this._data = {
-                code: element.code,
-                placement: element.id,
-                view: (viewId) ? viewId : this.uuid()
-            };
-            if (cookie) this._data["cookie"] = cookie;
-            element.viewed.forEach((a) => this._data[`strat-${a[0]}`] = a[1]);
-        }
-
-        get data(): Object {
-            return this._data;
-        }
-
-        /**
-             * Fast UUID generator, RFC4122 version 4 compliant.
-             * @author Jeff Ward (jcward.com).
-             * @license MIT license
-             * @link http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/21963136#21963136
-             **/
-
-        uuid() {
+    /**
+    * Fast UUID generator, RFC4122 version 4 compliant.
+    * @author Jeff Ward (jcward.com).
+    * @license MIT license
+    * @link http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/21963136#21963136
+    **/
+    export class UUID {
+        constructor() {
             let self = {};
             let lut = []; for (let i = 0; i < 256; i++) { lut[i] = (i < 16 ? "0" : "") + (i).toString(16); }
             const d0 = Math.random() * 0xffffffff | 0;
@@ -41,6 +25,23 @@ namespace FrameWatcher {
                 lut[d1 & 0xff] + lut[d1 >> 8 & 0xff] + "-" + lut[d1 >> 16 & 0x0f | 0x40] + lut[d1 >> 24 & 0xff] + "-" +
                 lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & 0xff] + "-" + lut[d2 >> 16 & 0xff] + lut[d2 >> 24 & 0xff] +
                 lut[d3 & 0xff] + lut[d3 >> 8 & 0xff] + lut[d3 >> 16 & 0xff] + lut[d3 >> 24 & 0xff];
+        }
+    }
+
+    export class Data {
+        private _data: Object;
+        constructor(element: Element, cookie: string, viewId: string) {
+            this._data = {
+                code: element.code,
+                placement: element.id,
+                view: (viewId) ? viewId : new UUID()
+            };
+            if (cookie) this._data["cookie"] = cookie;
+            element.viewed.forEach((a) => this._data[`strat-${a[0]}`] = a[1]);
+        }
+
+        get data(): Object {
+            return this._data;
         }
     }
 
